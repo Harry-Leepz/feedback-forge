c17547b9c9e6d28df1685b7b02ffe5165bdff1a3
Harry Dhillon <hdhillon478@gmail.com>
2024-06-12 22:01:21 +0100

    Move app into components folder

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import HashtagList from "./hashtag/HashtagList";
+import Container from "./layout/Container";
+import Footer from "./layout/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Footer />
+
+      <Container />
+
+      <HashtagList />
+    </>
+  );
+}
