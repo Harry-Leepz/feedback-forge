54a8e7401dfbc44589248e852b1a17c76cbdecac
Harry Dhillon <hdhillon478@gmail.com>
2024-06-13 16:05:00 +0100

    Add header sub components

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,18 @@
+import FeedbackForm from "../feedback/FeedbackForm";
+import Logo from "../shared/Logo";
+import PageHeading from "../shared/PageHeading";
+import Pattern from "../shared/Pattern";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Pattern />
+
+      <Logo />
+
+      <PageHeading />
+
+      <FeedbackForm />
+    </header>
+  );
 }
