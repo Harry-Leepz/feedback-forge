79464e6830b07adf40967344d4a10b105e74920c
Harry Dhillon <hdhillon478@gmail.com>
2024-06-15 22:23:33 +0100

    Hardcode boilerplate feedback list component

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,3 +1,31 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
 export default function FeedbackList() {
-  return <ol className='feedback-list'>FeedbackList</ol>;
+  return (
+    <ol className='feedback-list'>
+      <li className='feedback'>
+        <button>
+          <TriangleUpIcon />
+
+          <span>593</span>
+        </button>
+
+        <div>
+          <p>B</p>
+        </div>
+
+        <div>
+          <p>Harry</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            quibusdam sapiente, illum aliquam ipsam placeat tempore aspernatur,
+            quidem iste nam vitae repellat quis incidunt quasi laboriosam
+            nostrum perspiciatis molestias iure!
+          </p>
+        </div>
+
+        <p>4d ago</p>
+      </li>
+    </ol>
+  );
 }
