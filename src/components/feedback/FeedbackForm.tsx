5299a061bafc1433ea6e1707f44ff33c7aabcd26
Harry Dhillon <hdhillon478@gmail.com>
2024-06-16 12:49:58 +0100

    Add text input change handler and logic to calculate text char count

--- src/components/feedback/FeedbackForm.tsx
+++ src/components/feedback/FeedbackForm.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
+import { MAX_CHAR_COUNT } from "../../lib/contsants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const textCharCount = MAX_CHAR_COUNT - text.length;
+
   return (
     <form className='form'>
-      <textarea id='feedback-textarea' placeholder='test' spellCheck={false} />
+      <textarea
+        onChange={onTextChange}
+        value={text}
+        id='feedback-textarea'
+        placeholder='test'
+        spellCheck={false}
+      />
       <label htmlFor='feedback-textarea'>
         Enter your feedback here, remember to hashtag the company.
       </label>
 
       <div>
-        <p className='u-italic'>150</p>
+        <p className='u-italic'>{textCharCount}</p>
         <button>
           <span>Submit</span>
         </button>
