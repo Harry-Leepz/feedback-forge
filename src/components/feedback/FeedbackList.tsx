571f9bac9442a58b057586d6c29d003093bf8dc0
Harry Dhillon <hdhillon478@gmail.com>
2024-06-16 13:58:05 +0100

    Add loading state and conditional rendering of loading component

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
 import FeedbackItem from "./FeedbackItem";
+import Loading from "../shared/Loading";
 import { TFeedbackItem } from "../../lib/types";
 
 export default function FeedbackList() {
   const [feedbacks, setFeedbacks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFeedback = async () => {
@@ -17,6 +19,8 @@ export default function FeedbackList() {
         setFeedbacks(data.feedbacks);
       } catch (error) {
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchFeedback();
@@ -24,6 +28,7 @@ export default function FeedbackList() {
 
   return (
     <ol className='feedback-list'>
+      {loading && <Loading />}
       {feedbacks.map((feedback: TFeedbackItem) => (
         <FeedbackItem key={feedback.id} feedback={feedback} />
       ))}
