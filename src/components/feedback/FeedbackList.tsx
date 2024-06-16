d8501d6a82c209376a71d3f23e1164071d56058e
Harry Dhillon <hdhillon478@gmail.com>
2024-06-16 16:21:42 +0100

    Add error handling in fetch request

--- src/components/feedback/FeedbackList.tsx
+++ src/components/feedback/FeedbackList.tsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Loading from "../shared/Loading";
 import { TFeedbackItem } from "../../lib/types";
+import ErrorMessage from "../shared/ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbacks, setFeedbacks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchFeedback = async () => {
@@ -14,11 +16,14 @@ export default function FeedbackList() {
         const response = await fetch(
           "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
         );
+        if (!response.ok) {
+          throw new Error("An error occurred while fetching feedbacks.");
+        }
         const data = await response.json();
         console.log(data.feedbacks);
         setFeedbacks(data.feedbacks);
       } catch (error) {
-        console.error(error);
+        setError("An error occurred while fetching feedbacks.");
       } finally {
         setLoading(false);
       }
@@ -29,6 +34,7 @@ export default function FeedbackList() {
   return (
     <ol className='feedback-list'>
       {loading && <Loading />}
+      {error && <ErrorMessage errorMessage={error} />}
       {feedbacks.map((feedback: TFeedbackItem) => (
         <FeedbackItem key={feedback.id} feedback={feedback} />
       ))}
